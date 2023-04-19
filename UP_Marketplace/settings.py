72de71940c6d30ffe8c125c966798066425414db
Tristan <tlbachini@up.edu.ph>
2023-04-19 13:48:51 +0800

    inserted thesis files

--- UP_Marketplace/settings.py
+++ UP_Marketplace/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login_app',
-    'navbar',
 ]
 
 MIDDLEWARE = [
--- UP_Marketplace/urls.py
+++ UP_Marketplace/urls.py
@@ -19,6 +19,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("login_app.urls")),
-    path('', include("navbar.urls")),
 
 ]
