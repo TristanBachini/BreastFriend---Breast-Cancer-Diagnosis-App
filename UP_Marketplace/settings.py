49984de71b880723e550335bc75f8c30053d6eee
Tristan <tlbachini@up.edu.ph>
2023-04-20 22:12:41 +0800

    revert to previous commit

--- UP_Marketplace/settings.py
+++ UP_Marketplace/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'login_app',
+    'navbar',
 ]
 
 MIDDLEWARE = [
@@ -75,6 +76,18 @@ WSGI_APPLICATION = 'UP_Marketplace.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'Marketplace',
+        'USER': 'root',
+        'PASSWORD': 'Iamtheking27',
+        'PORT': 3306,
+        'HOST': '127.0.0.1',
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
